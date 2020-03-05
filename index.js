fb728387f301017c6b2018ae7a6cfa87937dc04a
Chris Carr <christocarr@outlook.com>
2020-03-05 11:01:26 +0000

    Fix typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function loadDoc() {
-  const key = 2003fc368e5207dda7404e4e4f733e3b
+  const key = '2003fc368e5207dda7404e4e4f733e3b'
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -8,6 +8,6 @@
       console.log(this.status)
     }
   };
-  xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/42", true);
+  xhttp.open("GET", "https://api.darksky.net/forecast/2003fc368e5207dda7404e4e4f733e3b/37.8267,-122.4233", true);
   xhttp.send();
 })()
