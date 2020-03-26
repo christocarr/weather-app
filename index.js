da6020959a85144f2daf3ecd499bbad5556ba5ea
Chris Carr <christocarr@outlook.com>
2020-03-26 16:05:28 +0000

    Add axios get request

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-(function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      //  document.getElementById("demo").innerHTML = this.responseText;
-      // console.log(this.responseText)
-      console.log(this.status)
-    }
-  };
-  xhttp.open("GET", "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22", true);
-  xhttp.send();
-})()
+
+// axios.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
+//   .then(res => {
+//     console.log(res)
+// }
+
+
+axios({
+  method: 'get',
+  url: 'https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22',
+  headers: {'Access-Control-Allow-Origin': '*'}
+}).then(res => {
+  console.log(res)
+})
