208eeb1ad21ae74fa8c27e4692a6492e6542ab46
Chris Carr <christocarr@outlook.com>
2020-03-30 16:19:18 +0100

    Delete axios from client

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-
-// axios.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
-//   .then(res => {
-//     console.log(res)
-// }
-
-
-axios({
-  method: 'get',
-  url: 'https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22',
-  headers: {'Access-Control-Allow-Origin': '*'}
-}).then(res => {
-  console.log(res)
-})
+(function() {
+  
+})()
