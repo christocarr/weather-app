fbeccbe601749681817dfaa68052b5fca0be2d5d
Chris Carr <christocarr@outlook.com>
2020-03-30 17:37:19 +0100

    Add IIFE to return data

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 (function() {
-  
+  async function getData() {
+    let data = await fetch('http://localhost:3000/')
+    .then(res => {
+      return res.json()
+    })
+    return data
+  }
+  console.log(getData())
 })()
