c29e15a05b5c89d439ce51e267a28f1916e8a985
Chris Carr <christocarr@outlook.com>
2020-03-30 17:36:59 +0100

    Add cors middleware

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const axios = require('axios')
-const fetch = require('node-fetch')
+const axios = require('axios');
+const fetch = require('node-fetch');
 
-app.get('/', async(req, res) => {
-  const fetch_res = await fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
-  const json = await fetch_res.json()
-  res.json(json)
-})
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', async (req, res) => {
+  const fetch_res = await fetch(
+    'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
+  );
+  const json = await fetch_res.json();
+  res.json(json);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
