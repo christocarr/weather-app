dfe05b2b234c42f0cbbba834384766bc3e6187a4
Chris Carr <christocarr@outlook.com>
2020-03-31 17:00:32 +0100

    Add request for params to get lat long from client

--- server/index.js
+++ server/index.js
@@ -6,16 +6,15 @@ const fetch = require('node-fetch');
 
 const APIKey = 'aaf81a2a5005fe5f583606e78b54cfd0'
 
-// lat long should come from client as params
-const lat = '51.560111'
-const long = '-0.297035'
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.get('/', async (req, res) => {
+app.get('/:latlong', async (req, res) => {
+  const latlong = req.params.latlong.split(',')
+  const lat = latlong[0]
+  const long = latlong[1]
   const fetch_res = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${APIKey}`
   );
