f6f17327731fdc9925e5e9a78f7c39d0f6ec70fe
Chris Carr <christocarr@outlook.com>
2020-04-01 13:43:15 +0100

    Add max temperature range

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 (function() {
-
-  const celsiusMinPara = document.getElementById('celsiusMinPara')
-  const fahrenheitMinPara = document.getElementById('fahrenheitMinPara')
+  const celsiusMinPara = document.getElementById('celsiusMinPara');
+  const fahrenheitMinPara = document.getElementById('fahrenheitMinPara');
+  const celsiusMaxPara = document.getElementById('celsiusMaxPara');
+  const fahrenheitMaxPara = document.getElementById('fahrenheitMaxPara');
 
   //get devices current location
   if (!navigator.geolocation) {
-    return `Geolocation is not supported by your browser.`
+    return `Geolocation is not supported by your browser.`;
   } else {
-    navigator.geolocation.getCurrentPosition(sendLocation)
+    navigator.geolocation.getCurrentPosition(sendLocation);
   }
 
   function sendLocation(position) {
-    const lat = position.coords.latitude
-    const long = position.coords.longitude
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
 
     async function getData(lat, long) {
-      const response = await fetch(`http://localhost:3000/${lat},${long}`)
-      return await response.json()
+      const response = await fetch(`http://localhost:3000/${lat},${long}`);
+      return await response.json();
     }
 
     getData(lat, long).then(data => {
+      console.log(data);
 
-      console.log(data)
+      const celsiusMin = Math.round(data.main.temp_min);
+      const fahrenheitMin = Math.round((celsiusMin * 9) / 5 + 32);
 
-      const celsiusMin = Math.round(data.main.temp_min)
-      const fahrenheitMin = Math.round(((celsiusMin * 9) / 5) + 32)
+      const celsiusMax = Math.round(data.main.temp_max);
+      const fahrenheitMax = Math.round((celsiusMax * 9) / 5 + 32)
 
       celsiusMinPara.innerHTML = `Min ${celsiusMin}&deg;C`;
       fahrenheitMinPara.innerHTML = `Min ${fahrenheitMin}&deg;F`;
-    })
+
+      celsiusMaxPara.innerHTML = `Max ${celsiusMax}&deg;C`;
+      fahrenheitMaxPara.innerHTML = `Max ${fahrenheitMax}&deg;F`;
+    });
   }
-  
-})()
+})();
